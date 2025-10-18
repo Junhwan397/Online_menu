4cefe592b2c7d64960de1655a93b1b9ca83b5fac
Junhwan Lee <j.lee36@ncl.ac.uk>
2025-10-19 02:51:21 +0900

    delete price info from modal

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from menu_app import create_app, db
 
 # 애플리케이션 팩토리를 사용하여 앱 인스턴스 생성
+#http://127.0.0.1:5000/menu/GTAJ7AE6
 app = create_app()
 
-# flask run 또는 python app.py 명령으로 실행하기 위함
 if __name__ == '__main__':
     app.run(debug=True)
 
